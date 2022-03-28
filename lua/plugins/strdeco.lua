c4535bed4c2d2c78649423b9c854e6aedb5c099c
tacogips <me@tacogips.me>
2022-03-28 10:54:26 +0900

    somE

--- lua/plugins/strdeco.lua
+++ lua/plugins/strdeco.lua
@@ -32,5 +32,20 @@ require("strdeco").setup({
 				return "pub mod " .. line .. ";"
 			end,
 		},
+
+		rs_mod_to_use = {
+			convert = function(line, line_no, line_count)
+				line = string.gsub(line, "^.*mod%s", "pub use ")
+				line = string.gsub(line, ";", "::*;")
+				return line
+			end,
+		},
+
+		tail_semicolon = {
+			convert = function(line, line_no, line_count)
+				line = string.gsub(line, "$", ";")
+				return line
+			end,
+		},
 	},
 })
