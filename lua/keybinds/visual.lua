ae89ab5b2fb73192dbed59b559a89b9728a3e1ad
tacogips <me@tacogips.me>
2022-05-15 23:00:36 +0900

    change strdeco

--- lua/keybinds/visual.lua
+++ lua/keybinds/visual.lua
@@ -1,3 +1,4 @@
 local kmap = require("utils.keymap")
 
 kmap.v("kj", "<ESC><ESC>")
+kmap.v(".e", ":StrDeco<CR>")
--- lua/plugins/strdeco.lua
+++ lua/plugins/strdeco.lua
@@ -2,28 +2,6 @@ require("strdeco").setup({
 
 	custom_conversion = {
 
-		custom_conv = {
-			before = function()
-				return "ah .."
-			end,
-			convert = function(line, line_no, line_count)
-				if string.match(line, "^%s*$") then
-					return nil
-				end
-				line = string.gsub(line, "python", function(s)
-					return "julia"
-				end)
-
-				line = string.gsub(line, "vim", function(s)
-					return "neovim"
-				end)
-				return line
-			end,
-			after = function()
-				return "now."
-			end,
-		},
-
 		rs_mod_list = {
 			convert = function(line, line_no, line_count)
 				line = string.gsub(line, "^.*%s", "")
