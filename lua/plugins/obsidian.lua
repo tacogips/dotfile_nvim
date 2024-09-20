0b9ac2688ade16fedfb1845a7d224eecb6ff8bfd
tacogips <me@tacogips.me>
2024-09-20 15:27:57 +0900

    asdd

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -14,6 +14,20 @@ require("obsidian").setup({
 	follow_url_func = function(url)
 		vim.fn.jobstart({ "firefox", url }) -- Mac OS
 	end,
+
+	note_id_func = function(title)
+		local prefix = ""
+		if title ~= nil then
+			-- If title is given, transform it into valid file name.
+			prefix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+		else
+			-- If title is nil, just add 4 random uppercase letters to the prefix.
+			for _ = 1, 4 do
+				prefix = prefix .. string.char(math.random(65, 90))
+			end
+		end
+		return prefix .. "-" .. tostring(os.time())
+	end,
 })
 
 cmd([[
@@ -24,5 +38,6 @@ cmd([[
 	au FileType markdown nmap ,t <ESC>:ObsidianToday<CR>
 	au FileType markdown nmap ,e <ESC>:ObsidianTags<CR>
 	au FileType markdown nmap ,r <ESC>:ObsidianBacklinks<CR>
+	au FileType markdown nmap ,o <ESC>:ObsidianOpen<CR>
 
 ]])
