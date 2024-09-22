164636bbeae8105fe742c2400b0cc55246f27336
tacogips <me@tacogips.me>
2024-09-22 13:17:07 +0900

    update

--- lua/plugins/lsp/typescript.lua
+++ lua/plugins/lsp/typescript.lua
@@ -1,6 +1,6 @@
 local lsp_util = require("plugins.lsp.lsp_util")
 local nvim_lsp = require("lspconfig")
-local name = "tsserver"
+local name = "ts_ls"
 
 lsp_util:setup_lsp(name, {
 	root_dir = nvim_lsp.util.root_pattern("package.json"),
