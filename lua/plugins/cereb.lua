56f99261869b8fa1464d3c1b6de9b6ed7a284497
tacogips <me@tacogips.me>
2024-10-07 11:34:32 +0900

    change

--- lua/plugins/cereb.lua
+++ lua/plugins/cereb.lua
@@ -4,9 +4,8 @@ local cmd = vim.cmd
 
 cmd([[
 	au FileType * nmap ,p <ESC>:CerebCurrentLineMd<CR>
-	au FileType * imap ,p <ESC>:CerebCurrentLineMd<CR>
 	au FileType * vmap ,p :CerebSelMd<CR>
-	au FileType * nmap ,; :CerebPageMd<CR>
-	au FileType * imap ,; :CerebPageMd<CR>
+	au FileType * nmap ,; :CerebPageMdWithQuery<CR>
+	au FileType * nmap ,] :CerebPageMd<CR>
 
 ]])
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -31,3 +31,16 @@ require("nvim-treesitter.configs").setup({
 		additional_vim_regex_highlighting = false,
 	},
 })
+
+--disabling syntax highlighting for markdown
+--resource: https://github.com/nvim-treesitter/nvim-treesitter/issues/5751#issuecomment-2311310008
+require("vim.treesitter.query").set(
+	"markdown",
+	"highlights",
+	[[
+;From MDeiml/tree-sitter-markdown
+[
+  (fenced_code_block_delimiter)
+] @punctuation.delimiter
+]]
+)
